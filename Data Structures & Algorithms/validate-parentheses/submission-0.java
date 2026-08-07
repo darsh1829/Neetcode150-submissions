c89ddd41a7f33ddb4d7c850e7dc1b42b6f6ceaab
Darsh Padaria <67645462+darsh1829@users.noreply.github.com>
2026-08-07 13:04:49 -0400

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+            
+            if(c=='{' || c=='(' || c=='[') {
+                stack.push(c);
+            } else { 
+                char top = stack.pop();
+            if((c=='}' && top!='{'|| c==')' && top!='(' || c==']' && top!='[')){
+                return false;
+            }}
+            
+        }
+        return stack.isEmpty();
+    }
+}
